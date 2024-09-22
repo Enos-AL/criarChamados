ad1b1c3f1f4ae52185b287847947842e6e8d4c5c
Enos-AL <ciscopackettracent@gmail.com>
2024-09-21 23:41:38 -0300

    Configurando o index e estilizando o index

--- backend/src/routes/usuarioRoutes.ts
+++ backend/src/routes/usuarioRoutes.ts
@@ -2,20 +2,38 @@
 import { Router } from 'express';
 import { criarTabelas } from '../controller/criarTabelas';
 import { body } from 'express-validator';
-import { obterColunasPermitidasChamados, obterColunasPermitidasAtualizacao } from '../config/config'; // Importe a função
+import { obterColunasPermitidasChamados, obterColunasPermitidasAtualizacao, verificarPermissao } from '../config/config'; // Importe a função
 
 const router = Router();
 
+// Rota para verificar a senha
+router.post('/verificarSenha', (req, res) => {
+    const { senha } = req.body;
+
+    if (!verificarPermissao(senha)) {
+        return res.status(401).json({ message: 'Senha incorreta' }); // Resposta se a senha estiver incorreta
+    }
+
+    res.status(200).json({ message: 'Senha correta' }); // Resposta se a senha estiver correta
+});
+
+// Rota para criar tabelas
 router.post('/criarTabelas', [
     body('senha').trim().isLength({ min: 8 }).withMessage('A senha deve ter pelo menos 8 caracteres.'),
     body('dados').isObject().withMessage('Os dados devem ser um objeto.'),
 ], criarTabelas);
 
-// Nova rota para obter colunas permitidas
+// Rota para obter colunas permitidas
 router.get('/colunas-permitidas', (req, res) => {
-    const colunasChamados = obterColunasPermitidasChamados(); // Obtenha colunas permitidas
+    const senha = req.query.senha as string | undefined; // Use 'as string | undefined' para manter a compatibilidade
+    if (!senha || !verificarPermissao(senha)) { // Verifica se a senha é válida
+        return res.status(401).json({ message: 'Senha incorreta. Acesso negado.' });
+    }
+
+    const colunasChamados = obterColunasPermitidasChamados();
     const colunasAtualizacao = obterColunasPermitidasAtualizacao();
-    res.json({ colunasChamados, colunasAtualizacao }); // Retorne as colunas como JSON
+    res.json({ colunasChamados, colunasAtualizacao });
 });
+
 
 export default router;
